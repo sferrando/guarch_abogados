455cbfa4c97341f2a7f01738c233345d59320a6f
Sergio <sferrando.92@gmail.com>
2017-07-29 21:24:07 +0200

    add change on mail

--- mail.php
+++ mail.php
@@ -5,7 +5,9 @@
   $mensaje.= "\nTelefono: ". $_POST['telephone'];
   $mensaje.= "\nMensaje: \n".$_POST['message'];
   $destino= "alberto@guarchabogados.com";
+  $info= "info@guarchabogados.com";
   $remitente = $_POST['email'];
   $asunto = "Mensaje enviado por: ".$_POST['name'];
   mail(utf8_decode($destino),utf8_decode($asunto),utf8_decode($mensaje),"FROM: $remitente");
+  mail(utf8_decode($info),utf8_decode($asunto),utf8_decode($mensaje),"FROM: $remitente");
 ?>
