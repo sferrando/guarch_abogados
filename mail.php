5e26acfd8bf794732f09f512b9f4575d7698ea82
Sergio <sferrando.92@gmail.com>
2017-08-01 00:34:15 +0200

    improve mail

--- mail.php
+++ mail.php
@@ -21,22 +21,21 @@
       <meta name="googlebot" content="noindex">
     </head>
     <div style="font-size: 14px; font-family: verdana; background-color: #c0d4e0; width: 100%; height: 800px; padding: 3em;"><img id="spin_to_win" src="http://guarchabogados.com/images/marca.png" alt="Guarch Abogados" style="display: block; margin: auto; margin-bottom: 20px;" />
-      <div style="width: 600px; background-color: #fff; border-radius: 10px; padding: 2em; display: block; margin: auto;">
-      <h3>Gracias por su confianza</h3>
-      <p>Guarch Abogados les da la bienvenida a su página Web. Le ofrecemos nuestros servicios para resolver cualquier problema legal que nos quiera plantear. Somos un despacho multidisciplinar con amplia experiencia en la reclamación de lesiones y daños derivados de accidentes de tráfico. Atenderemos y resolveremos de la misma manera cualquier duda que nos plantee en el ámbito del Derecho de Familia, Penal, Inmobiliario, Laboral y Administrativo. Por último, le ofrecemos la posibilidad de la mediación como un medio de resolución del conflicto diferente y alejado de la vía judicial. Darles nuestro agradecimiento por haber accedido a nuestra web y conocernos. Esperamos poder contar en breve con su confianza.</p>
-
-      <p> Por ultimo nos gustaria pedirle si podria compartir su experiencia con nosotros con una pequenna reseña en Google, rellearla es muy simple y lo podra hacer desde aqui: </p>
-      <p style="text-align: center;">
-        <a href="https://goo.gl/BQauxM" style="background-color: #c0d4e0;  display: inline-block; padding: 15px 32px; margin-bottom: 0; font-size: 18px; font-weight: 800;  text-align: center;  cursor: pointer; text-decoration: none; border: 1px solid transparent; border-radius: 4px; color: black;">
-          Rellenar Reseña</a>
-      </p>
-      <p>Saludos!</p>
-      <strong>Att. Alberto Guarch Brisa</strong>
+      <div style="width: 600px; background-color: #fff; border-radius: 10px; padding: 2em; display: block; margin: auto;text-align: center;">
+        <h4>Gracias por contactar con nosotros!</h4>
+        <p>Intentaremos atender y resolver su consulta con la mayor brevedad posible.</p>
+        <p>Att. Guarch Abogados</p>
+        <hr>
+        <p> Calle Garrigues nº17
+          <br/> 1er Piso, 2ª Puerta
+          <br>46001,Valencia,España
+          <br>+34 961 047 508
+        </p>
       </div>
     </div>';
 $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
 $headers .= 'To: '.$_POST['name'].'<'.$_POST['email'].'>' . "\r\n";
 $headers .= 'From: Guarch Abogados <contacto@guarchabogados.com>' . "\r\n";
-mail(utf8_decode($info),utf8_decode($asunto),utf8_decode($mail_de_confirmacion), $headers);
+mail(utf8_decode($destino),utf8_decode('Consulta recibida'),utf8_decode($mail_de_confirmacion), $headers);
 ?>
